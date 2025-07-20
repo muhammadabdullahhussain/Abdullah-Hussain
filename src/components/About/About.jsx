6702dd99b17e3117076274aee49e30503bad87be
muhammadabdullahhussain <muhammadabdullahhussain639@gmail.com>
2025-07-20 06:57:10 -0700

    add resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,8 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="#" // resume link
+          download={true}
+            href="/abdullahHussainPortFolio/src/assets/resume/Abdullah-Hussain-Resume.pdf" // resume link
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
